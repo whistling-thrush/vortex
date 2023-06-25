44b072ce4696da16c13ed468b0b0ea8a727a14d8
Hiren Jain <hiren.a.jain+github@gmail.com>
2023-06-25 10:58:59 +0800

    Add serial version UID for the two new panels

--- src/application/BookingHistory.java
+++ src/application/BookingHistory.java
@@ -4,6 +4,8 @@ import javax.swing.JPanel;
 
 public class BookingHistory extends JPanel {
 
+	private static final long serialVersionUID = -5347571249754630798L;
+
 	/**
 	 * Create the panel.
 	 */
--- src/application/CreateBooking.java
+++ src/application/CreateBooking.java
@@ -4,6 +4,8 @@ import javax.swing.JPanel;
 
 public class CreateBooking extends JPanel {
 
+	private static final long serialVersionUID = 7998788512651030012L;
+
 	/**
 	 * Create the panel.
 	 */
