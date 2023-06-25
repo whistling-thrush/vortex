7148e7481f1f7c5640be8251c15d808b8f61064d
Hiren Jain <hiren.a.jain+github@gmail.com>
2023-06-25 11:26:16 +0800

    Added component declarations in the classes

--- src/application/Dashboard.java
+++ src/application/Dashboard.java
@@ -6,12 +6,17 @@ import javax.swing.JToolBar;
 import javax.swing.JLabel;
 import javax.swing.SwingConstants;
 import java.awt.Font;
+import javax.swing.JSlider;
+import javax.swing.JScrollBar;
 
 public class Dashboard extends JPanel {
 
 	private static final long serialVersionUID = 1936925262291800888L;
 	
+	//Component declarations
 	private DeskBook deskBook;
+	private JLabel lblWelcome;
+	private JScrollBar scrollBar;
 
 	/**
 	 * Create the panel.
@@ -20,17 +25,21 @@ public class Dashboard extends JPanel {
 		this.deskBook = deskBook;
 		setLayout(null);
 		
-		JLabel lblNewLabel = new JLabel("Welcome, ...!");
-		lblNewLabel.setFont(new Font("Lucida Grande", Font.PLAIN, 30));
-		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
-		lblNewLabel.setBounds(198, 22, 424, 63);
-		add(lblNewLabel);
+		lblWelcome = new JLabel("Welcome, ...!");
+		lblWelcome.setFont(new Font("Lucida Grande", Font.PLAIN, 30));
+		lblWelcome.setHorizontalAlignment(SwingConstants.CENTER);
+		lblWelcome.setBounds(38, 6, 424, 63);
+		add(lblWelcome);
+		
+		scrollBar = new JScrollBar();
+		scrollBar.setBounds(430, 99, 64, 445);
+		add(scrollBar);
 		setupPanel();
 	}
 	
 	private void setupPanel() {
 		
-		setBounds(0, 0, 820, 446);
+		setBounds(0, 0, 500, 600);
 		
 	}
 }
--- src/application/LoginScreen.java
+++ src/application/LoginScreen.java
@@ -19,49 +19,71 @@ import java.awt.event.FocusAdapter;
 import java.awt.event.FocusEvent;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
 public class LoginScreen extends JPanel {
 	
 	private static final long serialVersionUID = -7247538218506533985L;
 	
+	//Component declarations
 	private DeskBook deskBook;
-	
-	
+	private JPasswordField fieldPass;
+	private SpringLayout layoutLogin;
+	private JLabel lblLogin;
+	private JLabel lblEmail;
+	private JFormattedTextField frmtdFieldEmail;
+	private JLabel lblValidEmail;
+	private JLabel lblPass;
+	private JLabel lblValidPass;
+	private JCheckBox chkBxShowPass;
+	private JSeparator separator;
+	private JButton btnForgotPass;
+	private JButton btnSignUp;
+	private JButton btnLogin;
 
 	/**
 	 * Create the panel.
 	 */
 	public LoginScreen(DeskBook deskBook) {
+		addKeyListener(new KeyAdapter() {
+			@Override
+			public void keyPressed(KeyEvent e) {
+				if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+					loginRequested();
+				}
+			}
+		});
 		this.deskBook = deskBook;
 		this.setBounds(0, 0, 500, 600);
 		setupPanel();
 	}
 	
 	private void setupPanel() {
-		SpringLayout sl_loginPanelLeft = new SpringLayout();
-		this.setLayout(sl_loginPanelLeft);
+		layoutLogin = new SpringLayout();
+		this.setLayout(layoutLogin);
 		
-		JLabel lblLogin = new JLabel("Log in.");
-		sl_loginPanelLeft.putConstraint(SpringLayout.SOUTH, lblLogin, 105, SpringLayout.NORTH, this);
-		sl_loginPanelLeft.putConstraint(SpringLayout.EAST, lblLogin, 189, SpringLayout.WEST, this);
+		lblLogin = new JLabel("Log in.");
+		layoutLogin.putConstraint(SpringLayout.SOUTH, lblLogin, 105, SpringLayout.NORTH, this);
+		layoutLogin.putConstraint(SpringLayout.EAST, lblLogin, 189, SpringLayout.WEST, this);
 		lblLogin.setLocation(185, 35);
-		sl_loginPanelLeft.putConstraint(SpringLayout.NORTH, lblLogin, 35, SpringLayout.NORTH, this);
-		sl_loginPanelLeft.putConstraint(SpringLayout.WEST, lblLogin, 60, SpringLayout.WEST, this);
+		layoutLogin.putConstraint(SpringLayout.NORTH, lblLogin, 35, SpringLayout.NORTH, this);
+		layoutLogin.putConstraint(SpringLayout.WEST, lblLogin, 60, SpringLayout.WEST, this);
 		lblLogin.setFont(new Font("Lucida Grande", Font.PLAIN, 26));
 		lblLogin.setHorizontalAlignment(SwingConstants.LEFT);
 		this.add(lblLogin);
 		
-		JLabel lblEmail = new JLabel("Email");
-		sl_loginPanelLeft.putConstraint(SpringLayout.WEST, lblEmail, 60, SpringLayout.WEST, this);
+		lblEmail = new JLabel("Email");
+		layoutLogin.putConstraint(SpringLayout.WEST, lblEmail, 60, SpringLayout.WEST, this);
 		lblEmail.setLocation(233, 115);
 		lblEmail.setHorizontalAlignment(SwingConstants.CENTER);
 		this.add(lblEmail);
 		
-		JFormattedTextField frmtdFieldEmail = new JFormattedTextField();
-		sl_loginPanelLeft.putConstraint(SpringLayout.NORTH, frmtdFieldEmail, 141, SpringLayout.NORTH, this);
-		sl_loginPanelLeft.putConstraint(SpringLayout.WEST, frmtdFieldEmail, 60, SpringLayout.WEST, this);
-		sl_loginPanelLeft.putConstraint(SpringLayout.EAST, frmtdFieldEmail, -53, SpringLayout.EAST, this);
-		sl_loginPanelLeft.putConstraint(SpringLayout.SOUTH, lblEmail, -6, SpringLayout.NORTH, frmtdFieldEmail);
+		frmtdFieldEmail = new JFormattedTextField();
+		layoutLogin.putConstraint(SpringLayout.NORTH, frmtdFieldEmail, 141, SpringLayout.NORTH, this);
+		layoutLogin.putConstraint(SpringLayout.WEST, frmtdFieldEmail, 60, SpringLayout.WEST, this);
+		layoutLogin.putConstraint(SpringLayout.EAST, frmtdFieldEmail, -53, SpringLayout.EAST, this);
+		layoutLogin.putConstraint(SpringLayout.SOUTH, lblEmail, -6, SpringLayout.NORTH, frmtdFieldEmail);
 		frmtdFieldEmail.setLocation(56, 145);
 		frmtdFieldEmail.addFocusListener(new FocusAdapter() {
 			@Override
@@ -79,56 +101,54 @@ public class LoginScreen extends JPanel {
 				}
 			}
 		});
-		
 		frmtdFieldEmail.setForeground(Color.LIGHT_GRAY);
 		frmtdFieldEmail.setSelectionColor(new Color(165, 205, 225));
 		frmtdFieldEmail.setText("Enter email address");
 		frmtdFieldEmail.setToolTipText("Enter your email");
 		this.add(frmtdFieldEmail);
 		
-		JLabel lblValidEmail = new JLabel("Enter valid email address!");
-		sl_loginPanelLeft.putConstraint(SpringLayout.NORTH, lblValidEmail, 0, SpringLayout.NORTH, lblEmail);
-		sl_loginPanelLeft.putConstraint(SpringLayout.WEST, lblValidEmail, 153, SpringLayout.EAST, lblEmail);
-		sl_loginPanelLeft.putConstraint(SpringLayout.EAST, lblValidEmail, 0, SpringLayout.EAST, frmtdFieldEmail);
+		lblValidEmail = new JLabel("Enter valid email address!");
+		layoutLogin.putConstraint(SpringLayout.NORTH, lblValidEmail, 0, SpringLayout.NORTH, lblEmail);
+		layoutLogin.putConstraint(SpringLayout.WEST, lblValidEmail, 153, SpringLayout.EAST, lblEmail);
+		layoutLogin.putConstraint(SpringLayout.EAST, lblValidEmail, 0, SpringLayout.EAST, frmtdFieldEmail);
 		lblValidEmail.setLocation(150, 119);
 		lblValidEmail.setHorizontalAlignment(SwingConstants.RIGHT);
 		lblValidEmail.setForeground(Color.RED);
 		this.add(lblValidEmail);
 		lblValidEmail.setVisible(false);
 		
-		JLabel lblPass = new JLabel("Password");
-		sl_loginPanelLeft.putConstraint(SpringLayout.NORTH, lblPass, 217, SpringLayout.NORTH, this);
-		sl_loginPanelLeft.putConstraint(SpringLayout.WEST, lblPass, 60, SpringLayout.WEST, this);
-		sl_loginPanelLeft.putConstraint(SpringLayout.SOUTH, frmtdFieldEmail, -34, SpringLayout.NORTH, lblPass);
+		lblPass = new JLabel("Password");
+		layoutLogin.putConstraint(SpringLayout.NORTH, lblPass, 217, SpringLayout.NORTH, this);
+		layoutLogin.putConstraint(SpringLayout.WEST, lblPass, 60, SpringLayout.WEST, this);
+		layoutLogin.putConstraint(SpringLayout.SOUTH, frmtdFieldEmail, -34, SpringLayout.NORTH, lblPass);
 		lblPass.setLocation(220, 197);
 		lblPass.setHorizontalAlignment(SwingConstants.CENTER);
 		this.add(lblPass);
 		
-		JPasswordField fieldPass = new JPasswordField();
-		sl_loginPanelLeft.putConstraint(SpringLayout.NORTH, fieldPass, 239, SpringLayout.NORTH, this);
-		sl_loginPanelLeft.putConstraint(SpringLayout.WEST, fieldPass, 60, SpringLayout.WEST, this);
-		sl_loginPanelLeft.putConstraint(SpringLayout.EAST, fieldPass, -53, SpringLayout.EAST, this);
-		sl_loginPanelLeft.putConstraint(SpringLayout.SOUTH, lblPass, -6, SpringLayout.NORTH, fieldPass);
+		fieldPass = new JPasswordField();
+		layoutLogin.putConstraint(SpringLayout.NORTH, fieldPass, 239, SpringLayout.NORTH, this);
+		layoutLogin.putConstraint(SpringLayout.WEST, fieldPass, 60, SpringLayout.WEST, this);
+		layoutLogin.putConstraint(SpringLayout.EAST, fieldPass, -53, SpringLayout.EAST, this);
+		layoutLogin.putConstraint(SpringLayout.SOUTH, lblPass, -6, SpringLayout.NORTH, fieldPass);
 		fieldPass.setLocation(56, 227);
 		fieldPass.setToolTipText("Enter your password");
 		this.add(fieldPass);
 		
-		JLabel lblValidPass = new JLabel("Enter valid password!");
-		sl_loginPanelLeft.putConstraint(SpringLayout.NORTH, lblValidPass, 0, SpringLayout.NORTH, lblPass);
-		sl_loginPanelLeft.putConstraint(SpringLayout.WEST, lblValidPass, 247, SpringLayout.WEST, this);
-		sl_loginPanelLeft.putConstraint(SpringLayout.EAST, lblValidPass, 0, SpringLayout.EAST, frmtdFieldEmail);
+		lblValidPass = new JLabel("Enter valid password!");
+		layoutLogin.putConstraint(SpringLayout.NORTH, lblValidPass, 0, SpringLayout.NORTH, lblPass);
+		layoutLogin.putConstraint(SpringLayout.WEST, lblValidPass, 247, SpringLayout.WEST, this);
+		layoutLogin.putConstraint(SpringLayout.EAST, lblValidPass, 0, SpringLayout.EAST, frmtdFieldEmail);
 		lblValidPass.setLocation(150, 201);
 		lblValidPass.setForeground(Color.RED);
 		lblValidPass.setHorizontalAlignment(SwingConstants.RIGHT);
 		this.add(lblValidPass);
 		lblValidPass.setVisible(false);
 		
-		JCheckBox chkBxShowPass = new JCheckBox("Show password");
-		sl_loginPanelLeft.putConstraint(SpringLayout.WEST, chkBxShowPass, 60, SpringLayout.WEST, this);
-		sl_loginPanelLeft.putConstraint(SpringLayout.SOUTH, fieldPass, -24, SpringLayout.NORTH, chkBxShowPass);
+		chkBxShowPass = new JCheckBox("Show password");
+		layoutLogin.putConstraint(SpringLayout.WEST, chkBxShowPass, 60, SpringLayout.WEST, this);
+		layoutLogin.putConstraint(SpringLayout.SOUTH, fieldPass, -24, SpringLayout.NORTH, chkBxShowPass);
 		chkBxShowPass.setLocation(185, 287);
 		chkBxShowPass.addActionListener(new ActionListener() {
-			
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				if (chkBxShowPass.isSelected()) {
@@ -142,68 +162,69 @@ public class LoginScreen extends JPanel {
 		chkBxShowPass.setToolTipText("Click to toggle show password");
 		this.add(chkBxShowPass);
 		
-		JSeparator separator = new JSeparator();
-		sl_loginPanelLeft.putConstraint(SpringLayout.NORTH, separator, 355, SpringLayout.NORTH, this);
-		sl_loginPanelLeft.putConstraint(SpringLayout.WEST, separator, 60, SpringLayout.WEST, this);
-		sl_loginPanelLeft.putConstraint(SpringLayout.EAST, separator, -53, SpringLayout.EAST, this);
-		sl_loginPanelLeft.putConstraint(SpringLayout.SOUTH, chkBxShowPass, -27, SpringLayout.NORTH, separator);
+		separator = new JSeparator();
+		layoutLogin.putConstraint(SpringLayout.NORTH, separator, 355, SpringLayout.NORTH, this);
+		layoutLogin.putConstraint(SpringLayout.WEST, separator, 60, SpringLayout.WEST, this);
+		layoutLogin.putConstraint(SpringLayout.EAST, separator, -53, SpringLayout.EAST, this);
+		layoutLogin.putConstraint(SpringLayout.SOUTH, chkBxShowPass, -27, SpringLayout.NORTH, separator);
 		separator.setLocation(56, 331);
 		this.add(separator);
 		
-		JButton btnForgotPass = new JButton("Forgot password?");
-		sl_loginPanelLeft.putConstraint(SpringLayout.NORTH, btnForgotPass, 474, SpringLayout.NORTH, this);
-		sl_loginPanelLeft.putConstraint(SpringLayout.WEST, btnForgotPass, 44, SpringLayout.WEST, this);
-		sl_loginPanelLeft.putConstraint(SpringLayout.SOUTH, btnForgotPass, 497, SpringLayout.NORTH, this);
-		sl_loginPanelLeft.putConstraint(SpringLayout.EAST, btnForgotPass, 198, SpringLayout.WEST, this);
+		btnForgotPass = new JButton("Forgot password?");
+		layoutLogin.putConstraint(SpringLayout.NORTH, btnForgotPass, 474, SpringLayout.NORTH, this);
+		layoutLogin.putConstraint(SpringLayout.WEST, btnForgotPass, 44, SpringLayout.WEST, this);
+		layoutLogin.putConstraint(SpringLayout.SOUTH, btnForgotPass, 497, SpringLayout.NORTH, this);
+		layoutLogin.putConstraint(SpringLayout.EAST, btnForgotPass, 198, SpringLayout.WEST, this);
 		btnForgotPass.setLocation(173, 447);
 		btnForgotPass.setHorizontalAlignment(SwingConstants.LEADING);
 		btnForgotPass.setBorderPainted(false);
 		btnForgotPass.setBackground(SystemColor.window);
 		this.add(btnForgotPass);
 		
-		JButton btnSignUp = new JButton("Sign up");
-		sl_loginPanelLeft.putConstraint(SpringLayout.NORTH, btnSignUp, 471, SpringLayout.NORTH, this);
-		sl_loginPanelLeft.putConstraint(SpringLayout.EAST, btnSignUp, -39, SpringLayout.EAST, this);
+		btnSignUp = new JButton("Sign up");
+		layoutLogin.putConstraint(SpringLayout.NORTH, btnSignUp, 471, SpringLayout.NORTH, this);
+		layoutLogin.putConstraint(SpringLayout.EAST, btnSignUp, -39, SpringLayout.EAST, this);
 		btnSignUp.setHorizontalAlignment(SwingConstants.TRAILING);
 		btnSignUp.setLocation(204, 446);
 		btnSignUp.setBorderPainted(false);
 		btnSignUp.setBackground(SystemColor.window);
 		this.add(btnSignUp);
 		
-		JButton btnLogin = new JButton("Log in");
-		sl_loginPanelLeft.putConstraint(SpringLayout.WEST, btnLogin, 60, SpringLayout.WEST, this);
-		sl_loginPanelLeft.putConstraint(SpringLayout.EAST, btnLogin, -53, SpringLayout.EAST, this);
-		sl_loginPanelLeft.putConstraint(SpringLayout.SOUTH, separator, -26, SpringLayout.NORTH, btnLogin);
-		sl_loginPanelLeft.putConstraint(SpringLayout.NORTH, btnLogin, 396, SpringLayout.NORTH, this);
-		sl_loginPanelLeft.putConstraint(SpringLayout.SOUTH, btnLogin, -33, SpringLayout.NORTH, btnSignUp);
+		btnLogin = new JButton("Log in");
+		layoutLogin.putConstraint(SpringLayout.WEST, btnLogin, 60, SpringLayout.WEST, this);
+		layoutLogin.putConstraint(SpringLayout.EAST, btnLogin, -53, SpringLayout.EAST, this);
+		layoutLogin.putConstraint(SpringLayout.SOUTH, separator, -26, SpringLayout.NORTH, btnLogin);
+		layoutLogin.putConstraint(SpringLayout.NORTH, btnLogin, 396, SpringLayout.NORTH, this);
+		layoutLogin.putConstraint(SpringLayout.SOUTH, btnLogin, -33, SpringLayout.NORTH, btnSignUp);
 		btnLogin.setLocation(56, 358);
 		btnLogin.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
-				
-				
-
-				if (new String(fieldPass.getPassword()).equals("pass") && frmtdFieldEmail.getText().equals("email")) {
-					deskBook.showDash();
-				}
-				
-				if (frmtdFieldEmail.getText().isBlank() || frmtdFieldEmail.getText().isEmpty() || frmtdFieldEmail.getText().equals("Enter email address")) {
-					lblValidEmail.setVisible(true);
-				} else {
-					lblValidEmail.setVisible(false);
-				}
-				
-				if (fieldPass.getPassword().length == 0) {
-					lblValidPass.setVisible(true);
-				} else {
-					lblValidPass.setVisible(false);
-				}
+				loginRequested();
 			}
 		});
 		btnLogin.setToolTipText("Click to log in to your account");
 		btnLogin.setBackground(new Color(255, 255, 255));
 		btnLogin.setFont(new Font("Lucida Grande", Font.PLAIN, 18));
 		this.add(btnLogin);
+	}
+	
+	private void loginRequested() {
+		if (new String(fieldPass.getPassword()).equals("pass") && frmtdFieldEmail.getText().equals("email")) {
+			deskBook.showDash();
+		}
+		
+		if (frmtdFieldEmail.getText().isBlank() || frmtdFieldEmail.getText().isEmpty() || frmtdFieldEmail.getText().equals("Enter email address")) {
+			lblValidEmail.setVisible(true);
+		} else {
+			lblValidEmail.setVisible(false);
+		}
+		
+		if (fieldPass.getPassword().length == 0) {
+			lblValidPass.setVisible(true);
+		} else {
+			lblValidPass.setVisible(false);
+		}
 	}
 	
 }
