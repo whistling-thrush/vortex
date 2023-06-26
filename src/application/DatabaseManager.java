c6ceba033dae6ead5edc252a85af9a604abe4d52
Hiren Jain <hiren.a.jain+github@gmail.com>
2023-06-26 11:27:03 +0800

    Allowed DatabaseManager to connect to the SQL server using a JDBC driver

--- src/application/DatabaseManager.java
+++ src/application/DatabaseManager.java
@@ -4,7 +4,15 @@ import java.sql.*;
 public class DatabaseManager {
 	
 	public static void initialiseDBMS() {
+		String url = "jdbc:mysql://localhost:3306/desk_book";
+		String username = "MyUsername";
+		String password = "MyPassword";
 		
+		try {
+			Connection connection = DriverManager.getConnection(url, username, password);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 	}
 	
 }
